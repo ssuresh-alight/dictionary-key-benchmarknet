d5f3d999731c6547c29765511338591d4f55a787
Sebin Suresh <sebin.suresh@alight.com>
2024-11-25 18:20:06 -0600

    feat: change test scope to only read, fix issues w parameterization, extract to separate class and file

---
+++ BenchmarkMaps/DictionaryReadBenchmarks.cs
@@ -0,0 +1,83 @@
+﻿using AutoFixture;
+using BenchmarkDotNet.Attributes;
+
+using NamedTupleKey = (int? providerId, string? taxid, short? procedureCategoryCode);
+
+namespace BenchmarkMaps;
+
+public class DictionaryReadBenchmarks
+{
+    private readonly Fixture _fixture = new Fixture();
+
+    private Dictionary<string, Message> _stringMap;
+    private Dictionary<TestRecord, Message> _recordMap;
+    private Dictionary<NamedTupleKey, Message> _tupleMap;
+    private TestRecord _existingKeyData;
+    
+    public DictionaryReadBenchmarks()
+    {
+        var recordKeys = _fixture.CreateMany<TestRecord>(100).ToArray();
+
+        _existingKeyData = _fixture.Create<TestRecord>();
+        
+        _stringMap = [];
+        _recordMap = [];
+        _tupleMap = [];
+        
+        Array.ForEach(
+            recordKeys,
+            r =>
+            {
+                var message = _fixture.Create<Message>();
+                _stringMap[$"f{r.ProviderId}_t{r.TaxId}_p{r.PrimaryProcedureCategoryCode}"] =
+                    message;
+                _recordMap[r] = message;
+                _tupleMap[(
+                    providerId: r.ProviderId,
+                    taxid: r.TaxId,
+                    procedureCategoryCode: r.PrimaryProcedureCategoryCode
+                )] = message;
+            }
+        );
+        
+        var existingMessage = _fixture.Create<Message>();
+        _stringMap[$"f{_existingKeyData.ProviderId}_t{_existingKeyData.TaxId}_p{_existingKeyData.PrimaryProcedureCategoryCode}"] =
+            existingMessage;
+        _recordMap[_existingKeyData] = existingMessage;
+        _tupleMap[(
+            providerId: _existingKeyData.ProviderId,
+            taxid: _existingKeyData.TaxId,
+            procedureCategoryCode: _existingKeyData.PrimaryProcedureCategoryCode
+        )] = existingMessage;
+    }
+
+    [Benchmark(Baseline = true)]
+    public Message? Retrieving_Using_String_Key()
+    {
+        var key = $"f{_existingKeyData.ProviderId}_t{_existingKeyData.TaxId}_p{_existingKeyData.PrimaryProcedureCategoryCode}";
+        return _stringMap[key];
+    }
+
+    [Benchmark]
+    public Message? Retrieving_Using_RecordKey()
+    {
+        var key = new TestRecord()
+        {
+            ProviderId = _existingKeyData.ProviderId,
+            TaxId = _existingKeyData.TaxId,
+            PrimaryProcedureCategoryCode = _existingKeyData.PrimaryProcedureCategoryCode,
+        };
+        return _recordMap[key];
+    }
+
+    [Benchmark]
+    public Message? Retrieving_Using_TupleKey()
+    {
+        var key = (
+            providerId: _existingKeyData.ProviderId,
+            taxid: _existingKeyData.TaxId,
+            procedureCategoryCode: _existingKeyData.PrimaryProcedureCategoryCode
+        );
+        return _tupleMap[key];
+    }
+}
